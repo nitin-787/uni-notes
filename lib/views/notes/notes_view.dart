ad7968b3a6d7d73f2214229c215c9355f8de3066
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-07-13 10:05:05 +0530

    Creating New Notes

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -76,6 +76,7 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('Waiting for all notes...');
                     default:
                       return const CircularProgressIndicator();
