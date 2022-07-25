d4405d78cc125dcc65ad2c651091dd5322deae3b
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-07-25 12:14:01 +0530

    Loading Screen

---
+++ lib/helper/loading/loading_screen_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+typedef CloseLoadingScreen = bool Function();
+typedef UpdateLoadingScreen = bool Function(String text);
+
+@immutable
+class LoadingScreenController {
+  final CloseLoadingScreen close;
+  final UpdateLoadingScreen update;
+
+  const LoadingScreenController({
+    required this.close,
+    required this.update,
+  });
+}
