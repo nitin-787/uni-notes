3e5d31062318d402d810b58f7eaa68963d61793c
Nitin Sharma <76255199+nitin-787@users.noreply.github.com>
2022-07-28 19:21:57 +0530

    Re-generated the android folder

---
+++ android/app/src/main/java/com/nitin/mynotes/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nitin.mynotes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/nitin/mynotes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nitin.mynotes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
