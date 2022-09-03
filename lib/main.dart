0ec3f6fe4ce980e264ddcc3b575779eed4ee4d8e
aadarshraj4321 <aadarshraj4321@gmail.com>
2022-09-03 17:05:08 +0530

    changes in login UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ import 'package:mynotes/views/notes/create_update_note_view.dart';
 import 'package:mynotes/views/forgot_password_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
-import 'package:mynotes/views/verify_email_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 import 'package:mynotes/extentions/buildcontext/loc.dart';
 
 void main() {
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -6,6 +6,7 @@ import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
+import 'package:path/path.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -15,6 +16,8 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+
+  bool changeButton = false;
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -67,6 +70,7 @@ class _LoginViewState extends State<LoginView> {
                 Text(
                   context.loc.login_view_prompt,
                 ),
+                SizedBox(height: 20.0,),
                 TextField(
                   controller: _email,
                   enableSuggestions: false,
@@ -84,20 +88,59 @@ class _LoginViewState extends State<LoginView> {
                   decoration: InputDecoration(
                     hintText: context.loc.password_text_field_placeholder,
                   ),
+                  
+                ),SizedBox(height: 20.0,),
+                // TextButton(
+                //   onPressed: () async {
+                //     final email = _email.text;
+                //     final password = _password.text;
+                //     context.read<AuthBloc>().add(
+                //           AuthEventLogIn(
+                //             email,
+                //             password,
+                //           ),
+                //         );
+                        
+                //   },
+                //   child: Text(context.loc.login,
+                //    style: TextStyle(
+                //     fontWeight: FontWeight.bold,
+                //     fontSize: 16.0,),),
+                  
+                // ),
+
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+
+                  await Future.delayed(Duration(seconds: 1));
+                  final email = _email.text;
+                  final password = _password.text;
+                  context.read<AuthBloc>().add(
+                    AuthEventLogIn(email, password),
+                  );
+                },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  alignment: Alignment.center,
+                  width: changeButton ? 40 : 150,
+                  height: 40,
+                  child: changeButton ? Icon(Icons.done, color: Colors.white,) : Text("Login", style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold,
+                    fontSize: 14,),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(changeButton ? 40 : 8)
+                      ),
                 ),
-                TextButton(
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    context.read<AuthBloc>().add(
-                          AuthEventLogIn(
-                            email,
-                            password,
-                          ),
-                        );
-                  },
-                  child: Text(context.loc.login),
-                ),
+              ),
+
+              SizedBox(height: 20.0,),
+
                 TextButton(
                   onPressed: () {
                     context.read<AuthBloc>().add(
