b3d8723551a5c346bfb57e94d2ee610946a4b306
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-09-03 18:27:19 +0530

    enhancement: login ui

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -15,8 +15,8 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  bool changeButton = false;
-  bool secondChangeButton = false;
+  bool loginButton = false;
+  bool registerButton = false;
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -58,191 +58,176 @@ class _LoginViewState extends State<LoginView> {
         }
       },
       child: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text(context.loc.login),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/icon/login.png",
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text(
-                  "Welcome Login To Write Notes",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Colors.blueAccent),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: _email,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          hintText: context.loc.email_text_field_placeholder,
-                        ),
-                      ),
-                      TextField(
-                        controller: _password,
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        decoration: InputDecoration(
-                          hintText: context.loc.password_text_field_placeholder,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      InkWell(
-                        onTap: () async {
-                          setState(
-                            () {
-                              changeButton = true;
-                            },
-                          );
-                          await Future.delayed(
-                            const Duration(seconds: 1),
-                          );
-                          final email = _email.text;
-                          final password = _password.text;
-                          if (!mounted) return;
-                          context.read<AuthBloc>().add(
-                                AuthEventLogIn(email, password),
-                              );
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          alignment: Alignment.center,
-                          width: changeButton ? 40 : 150,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
-                            borderRadius:
-                                BorderRadius.circular(changeButton ? 40 : 8),
-                          ),
-                          child: changeButton
-                              ? const Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : const Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 110.0,
-                      ),
-                      InkWell(
-                        onTap: () async {
-                          setState(
-                            () {
-                              secondChangeButton = true;
-                            },
-                          );
-                          await Future.delayed(
-                            const Duration(seconds: 1),
-                          );
-                          if (!mounted) return;
-                          context.read<AuthBloc>().add(
-                                const AuthEventForgotPassword(),
-                              );
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          alignment: Alignment.center,
-                          width: changeButton ? 40 : 100,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
-                            borderRadius:
-                                BorderRadius.circular(changeButton ? 40 : 6),
-                          ),
-                          child: changeButton
-                              ? const Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : const Text(
-                                  "Forget Password",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 10,
-                                  ),
-                                ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      InkWell(
-                        onTap: () async {
-                          setState(() {
-                            secondChangeButton = true;
-                          });
-
-                          await Future.delayed(
-                            const Duration(seconds: 1),
-                          );
-                          if (!mounted) return;
-                          context.read<AuthBloc>().add(
-                                const AuthEventShouldRegister(),
-                              );
-                        },
-                        child: AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          alignment: Alignment.center,
-                          width: changeButton ? 40 : 150,
-                          height: 45,
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
-                            borderRadius:
-                                BorderRadius.circular(changeButton ? 45 : 6),
-                          ),
-                          child: changeButton
-                              ? const Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                )
-                              : const Text(
-                                  "Register",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                        ),
-                      ),
-                    ],
+        body: Container(
+          padding: const EdgeInsets.only(top: 20),
+          child: Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/icon/login.png",
+                    fit: BoxFit.cover,
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  const Text(
+                    "Welcome Login To Write Notes",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Colors.blueAccent),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 32.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _email,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: context.loc.email_text_field_placeholder,
+                          ),
+                        ),
+                        TextField(
+                          controller: _password,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          decoration: InputDecoration(
+                            hintText:
+                                context.loc.password_text_field_placeholder,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                context.read<AuthBloc>().add(
+                                      const AuthEventForgotPassword(),
+                                    );
+                              },
+                              child: const Text(
+                                style: TextStyle(
+                                  fontSize: 15.5,
+                                ),
+                                "forgot password?",
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 40.0,
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            setState(
+                              () {
+                                loginButton = true;
+                              },
+                            );
+                            await Future.delayed(
+                              const Duration(seconds: 1),
+                            );
+                            final email = _email.text;
+                            final password = _password.text;
+                            if (!mounted) return;
+                            context.read<AuthBloc>().add(
+                                  AuthEventLogIn(email, password),
+                                );
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 1),
+                            alignment: Alignment.center,
+                            width: loginButton ? 40 : 150,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                              borderRadius:
+                                  BorderRadius.circular(loginButton ? 40 : 8),
+                            ),
+                            child: loginButton
+                                ? const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : const Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40.0,
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            setState(
+                              () {
+                                registerButton = true;
+                              },
+                            );
+                            await Future.delayed(
+                              const Duration(seconds: 1),
+                            );
+                            final email = _email.text;
+                            final password = _password.text;
+                            if (!mounted) return;
+                            context.read<AuthBloc>().add(
+                                  AuthEventRegister(email, password),
+                                );
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(seconds: 1),
+                            alignment: Alignment.center,
+                            width: registerButton ? 40 : 150,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(
+                                  registerButton ? 40 : 8),
+                            ),
+                            child: registerButton
+                                ? const Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : const Text(
+                                    "Register",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
