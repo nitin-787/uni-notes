a9cda4f15a895f5264d1cd092c102db8d3153fb9
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-09-04 11:22:26 +0530

    added File upload feature

---
+++ lib/views/Public/file_upload.dart
@@ -0,0 +1,134 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FileUpload extends StatefulWidget {
+  const FileUpload({super.key});
+
+  @override
+  _FileUploadState createState() => _FileUploadState();
+}
+
+class _FileUploadState extends State<FileUpload> {
+  File? file;
+  ImagePicker image = ImagePicker();
+  String url = "";
+  var name;
+  var color1 = Colors.redAccent[700];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MaterialButton(
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              elevation: 5.0,
+              height: 60,
+              onPressed: () {
+                getfile();
+              },
+              color: Colors.blue,
+              child: const Text(
+                "Get file",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              elevation: 5.0,
+              height: 60,
+              onPressed: () {
+                getImage();
+              },
+              child: const Text(
+                "Get image",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getfile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'doc'],
+    );
+
+    if (result != null) {
+      File c = File(result.files.single.path.toString());
+      setState(() {
+        file = c;
+        name = result.names.toString();
+      });
+      uploadFile();
+    }
+  }
+
+  getImage() async {
+    var img = await image.pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = File(img!.path);
+    });
+    if (file != null) {
+      uploadFile();
+    }
+  }
+
+  uploadFile() async {
+    try {
+      var imagefile =
+          FirebaseStorage.instance.ref().child("Users").child("/$name");
+      UploadTask task = imagefile.putFile(file!);
+      TaskSnapshot snapshot = await task;
+      url = await snapshot.ref.getDownloadURL();
+
+      print(url);
+      if (url != null && file != null) {
+        Fluttertoast.showToast(
+          msg: "file uloaded sucessfully",
+          textColor: Colors.red,
+        );
+      } else {
+        Fluttertoast.showToast(
+          msg: "Something went wrong",
+          textColor: Colors.red,
+        );
+      }
+    } on Exception catch (e) {
+      Fluttertoast.showToast(
+        msg: e.toString(),
+        textColor: Colors.grey,
+      );
+    }
+  }
+}
