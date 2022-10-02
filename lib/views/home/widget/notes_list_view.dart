7c06445eb551ca7acab599800403ee9fb7908289
Mehul Kumar <202151092@iiitvadodara.ac.in>
2022-10-02 22:20:44 +0530

    Shows Animation only Once

--- lib/views/home/widget/notes_list_view.dart
+++ lib/views/home/widget/notes_list_view.dart
@@ -28,12 +28,18 @@ class NotesListView extends StatefulWidget {
 }
 
 class _NotesListViewState extends State<NotesListView> {
-  late bool _isLoading;
+  static late bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _isLoading = true;
+
+    if (_isLoading == false) {
+      _isLoading = false;
+    } else {
+      _isLoading = true;
+    }
+
     Future.delayed(const Duration(seconds: 2), () {
       setState(() {
         _isLoading = false;
@@ -165,8 +171,6 @@ class _NotesListViewState extends State<NotesListView> {
   }
 }
 
-
-
 class SkeletonNotes extends StatelessWidget {
   const SkeletonNotes({
     Key? key,
@@ -186,34 +190,59 @@ class SkeletonNotes extends StatelessWidget {
   }
 
   Widget buildShimmer() => Card(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-    child: Column(
-      children: [
-          Row(children:  [
-            const ShimmerWidget.circular(
-          height: 100,
-          width: 159,
-          shapeBorder: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.zero,
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.zero)),
-          ),
-          Expanded(child: Column(crossAxisAlignment: CrossAxisAlignment.start,children:  [
-            const ShimmerWidget.rectangular(height: 15, width: 200,),
-            const SizedBox(height: 15,),
-            const ShimmerWidget.rectangular(height: 10, width: 180,),
-            const SizedBox(height: 15,),
-            Row(children: const [ShimmerWidget.rectangular(height: 10, width: 150,),
-              SizedBox(width: 25,)
-            ,Icon(CupertinoIcons.arrow_down_square, color: Colors.grey,)],)
-            
-
-          ],).px8())])
-      ],
-    ),
-  );
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: [
+            Row(children: [
+              const ShimmerWidget.circular(
+                height: 100,
+                width: 159,
+                shapeBorder: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.zero,
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.zero)),
+              ),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ShimmerWidget.rectangular(
+                    height: 15,
+                    width: 200,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const ShimmerWidget.rectangular(
+                    height: 10,
+                    width: 180,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: const [
+                      ShimmerWidget.rectangular(
+                        height: 10,
+                        width: 150,
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      Icon(
+                        CupertinoIcons.arrow_down_square,
+                        color: Colors.grey,
+                      )
+                    ],
+                  )
+                ],
+              ).px8())
+            ])
+          ],
+        ),
+      );
 }
 
 class ShimmerWidget extends StatelessWidget {
@@ -221,8 +250,7 @@ class ShimmerWidget extends StatelessWidget {
   final double width;
   final shapeBorder;
 
-  const ShimmerWidget.rectangular(
-      {required this.width, required this.height})
+  const ShimmerWidget.rectangular({required this.width, required this.height})
       : this.shapeBorder = const RoundedRectangleBorder();
 
   const ShimmerWidget.circular({
@@ -230,9 +258,6 @@ class ShimmerWidget extends StatelessWidget {
     required this.width,
     this.shapeBorder = const CircleBorder(),
   });
-
-
-  
 
   @override
   Widget build(BuildContext context) => Shimmer.fromColors(
@@ -267,6 +292,3 @@ class Skeleton extends StatelessWidget {
         ),
       );
 }
-
-
-
