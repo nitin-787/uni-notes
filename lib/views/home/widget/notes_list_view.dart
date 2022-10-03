e5a6b8306f181167032f4228fca0af7b4b5d7d29
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-10-03 19:41:42 +0530

    sperated the shimmerLoading from noteListView and changed the file name

--- lib/views/home/widget/notes_list_view.dart
+++ lib/views/home/widget/notes_list_view.dart
@@ -1,25 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mynotes/config/size_config.dart';
 import 'package:mynotes/constants/colors.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:velocity_x/velocity_x.dart';
+import 'package:mynotes/helper/loading/shimmer_loading.dart';
 
 typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatefulWidget {
-  final Iterable<CloudNote> notes;
-  final NoteCallback onDeleteNote;
-  final NoteCallback onTap;
-
   const NotesListView({
     Key? key,
     required this.notes,
     required this.onDeleteNote,
     required this.onTap,
   }) : super(key: key);
+
+  final Iterable<CloudNote> notes;
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
 
   @override
   State<NotesListView> createState() => _NotesListViewState();
@@ -167,130 +165,4 @@ class _NotesListViewState extends State<NotesListView> {
             ],
           );
   }
-}
-
-class SkeletonNotes extends StatelessWidget {
-  const SkeletonNotes({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        return buildShimmer();
-      },
-      itemCount: 5,
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
-  }
-
-  Widget buildShimmer() => Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          children: [
-            Row(children: [
-              const ShimmerWidget.circular(
-                height: 100,
-                width: 159,
-                shapeBorder: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.zero,
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.zero)),
-              ),
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const ShimmerWidget.rectangular(
-                    height: 15,
-                    width: 200,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const ShimmerWidget.rectangular(
-                    height: 10,
-                    width: 180,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: const [
-                      ShimmerWidget.rectangular(
-                        height: 10,
-                        width: 150,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Icon(
-                        CupertinoIcons.arrow_down_square,
-                        color: Colors.grey,
-                      )
-                    ],
-                  )
-                ],
-              ).px8())
-            ])
-          ],
-        ),
-      );
-}
-
-class ShimmerWidget extends StatelessWidget {
-  final double height;
-  final double width;
-  final shapeBorder;
-
-  const ShimmerWidget.rectangular({
-    super.key,
-    required this.width,
-    required this.height,
-  }) : shapeBorder = const RoundedRectangleBorder();
-
-  const ShimmerWidget.circular({
-    super.key,
-    required this.height,
-    required this.width,
-    this.shapeBorder = const CircleBorder(),
-  });
-
-  @override
-  Widget build(BuildContext context) => Shimmer.fromColors(
-        baseColor: Colors.grey[400]!,
-        highlightColor: Colors.grey[300]!,
-        child: Container(
-          width: width,
-          height: height,
-          decoration:
-              ShapeDecoration(shape: shapeBorder, color: Colors.grey[400]!),
-        ),
-      );
-}
-
-class Skeleton extends StatelessWidget {
-  const Skeleton({
-    Key? key,
-    required this.height,
-    required this.width,
-  }) : super(key: key);
-
-  final double? height, width;
-  @override
-  Widget build(BuildContext context) => Shimmer.fromColors(
-        baseColor: Colors.grey[300]!,
-        highlightColor: Colors.grey[400]!,
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          height: height,
-          width: width,
-          color: Colors.grey,
-        ),
-      );
 }
