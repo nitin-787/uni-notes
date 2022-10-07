d2e5de07709fcd6e6f063a2e1e84657ad3e56d73
Nehal Jaisalmeria <nehal.jaisalmeria@sapidblue.com>
2022-10-07 17:17:18 +0530

    Completed Popups

--- lib/views/notes/note_data.dart
+++ lib/views/notes/note_data.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mynotes/config/size_config.dart';
+import 'package:mynotes/constants/colors.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_serivce.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storgae.dart';
 import 'package:mynotes/views/notes/notes_list_view.dart';
+import 'package:mynotes/widget/popup.dart';
 
 extension Count<T extends Iterable> on Stream<T> {
   Stream<int> get getLength => map((event) => event.length);
@@ -71,13 +74,100 @@ class _NoteDataState extends State<NoteData> {
     );
   }
 
-  _openDeleteNotePopup(note) {
+  _openDeleteNotePopup(CloudNote note) {
     showDialog(
       context: context,
       useSafeArea: true,
       builder: (context) {
-        return AlertDialog(
-          content: Text('hi'),
+        return Popup(
+          title: 'Do you want to delete?',
+          description: 'You can\'t restore this file',
+          imagePath: 'assets/icon/warning.png',
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                deleteNote(note);
+              },
+              child: Text(
+                'Delete',
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth(10),
+                  color: AppColors.backgroundColor,
+                ),
+              ),
+            ),
+            const SizedBox(width: 6),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(
+                  width: 1,
+                  color: AppColors.mainColor,
+                ),
+              ),
+              child: Text(
+                'Cancel',
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth(10),
+                  color: AppColors.mainColor,
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  deleteNote(CloudNote note) async {
+    await _notesService.deleteNote(documentId: note.documentId).then((value) {
+      Navigator.pop(context);
+      _noteDeleteSuccess();
+    }).onError((error, stackTrace) {
+      Navigator.pop(context);
+      _noteDeleteFailed();
+    });
+  }
+
+  _noteDeleteSuccess() {
+    showDialog(
+      context: context,
+      useSafeArea: true,
+      builder: (context) {
+        return Popup(
+          title: 'File Deleted',
+          description: 'That\'s all :)',
+          imagePath: 'assets/icon/success.png',
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Okay, thank you',
+                style: GoogleFonts.poppins(
+                  fontSize: screenWidth(10),
+                  color: AppColors.backgroundColor,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _noteDeleteFailed() {
+    showDialog(
+      context: context,
+      useSafeArea: true,
+      builder: (context) {
+        return const Popup(
+          title: 'Error',
+          description: 'Sorry',
+          imagePath: 'assets/icon/error.png',
         );
       },
     );
