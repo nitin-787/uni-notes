87eab63d84993c167983d6e78d0ee1b1e90a3d35
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-10-09 21:51:42 +0530

    feat: implement initial note editor view
    Signed-off-by: SP-XD <69642436+SP-XD@users.noreply.github.com>

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:mynotes/constants/colors.dart';
 import 'package:mynotes/extentions/buildcontext/loc.dart';
 import 'package:mynotes/services/auth/auth_serivce.dart';
 import 'package:mynotes/utilities/dialogs/cannot_share_empty_not_dialog.dart';
@@ -6,6 +8,7 @@ import 'package:mynotes/utilities/generic/get_argumnets.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storgae.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:flutter/src/widgets/text.dart' as TextFlutter;
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({Key? key}) : super(key: key);
@@ -18,6 +21,8 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   CloudNote? _note;
   late final FirebaseCloudStorage _notesSerivce;
   late final TextEditingController _textController;
+  QuillController _quillController = QuillController.basic();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
@@ -90,47 +95,112 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     super.dispose();
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          context.loc.note,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              final text = _textController.text;
-              if (_note == null || text.isEmpty) {
-                await showCannotShareEmptyNoteDialog(context);
-              } else {
-                Share.share(text);
-              }
-            },
-            icon: const Icon(Icons.share),
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColor1,
+          leading: IconButton(
+            icon:
+                Icon(Icons.arrow_back_ios_rounded, color: AppColors.textColor),
+            onPressed: () => Navigator.of(context).pop(),
           ),
-        ],
-      ),
-      body: FutureBuilder(
-        future: createOrGetExistingNote(context),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              _setupTextControllerListner();
-              return TextField(
-                controller: _textController,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                    hintText: context.loc.start_typing_your_note),
-              );
-            default:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-          }
-        },
-      ),
-    );
+          actions: [
+            Center(
+              child: TextFlutter.Text('Last Modified: 24/09/2022',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(color: AppColors.textColor1, fontSize: 16)),
+            ),
+            const SizedBox(width: 10)
+          ],
+          shadowColor: Colors.transparent,
+        ),
+        body: SafeArea(
+            child: Container(
+                decoration: BoxDecoration(color: AppColors.backgroundColor1),
+                // height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          TextField(
+                              controller: _textController,
+                              keyboardType: TextInputType.text,
+                              decoration: const InputDecoration(
+                                hintText: "Title",
+                              ),
+                              style: TextStyle(fontSize: 24)),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration:
+                            BoxDecoration(color: AppColors.backgroundColor1),
+                        child: QuillEditor(
+                          controller: _quillController,
+                          focusNode: _focusNode,
+                          autoFocus: false,
+                          scrollController: ScrollController(),
+                          scrollable: true,
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          readOnly: false,
+                          expands: true,
+                          placeholder: "Start typing...",
+                        ),
+                      ),
+                    ),
+                    QuillToolbar.basic(
+                      controller: _quillController,
+                      multiRowsDisplay: false,
+                      dialogTheme: QuillDialogTheme(),
+                    ),
+                  ],
+                )))
+        // appBar: AppBar(
+        //   title: Text(
+        //     context.loc.note,
+        //   ),
+        //   actions: [
+        //     IconButton(
+        //       onPressed: () async {
+        //         final text = _textController.text;
+        //         if (_note == null || text.isEmpty) {
+        //           await showCannotShareEmptyNoteDialog(context);
+        //         } else {
+        //           Share.share(text);
+        //         }
+        //       },
+        //       icon: const Icon(Icons.share),
+        //     ),
+        //   ],
+        // ),
+        // body: FutureBuilder(
+        //   future: createOrGetExistingNote(context),
+        //   builder: (context, snapshot) {
+        //     switch (snapshot.connectionState) {
+        //       case ConnectionState.done:
+        //         _setupTextControllerListner();
+        //         return TextField(
+        //           controller: _textController,
+        //           keyboardType: TextInputType.multiline,
+        //           maxLines: null,
+        //           decoration: InputDecoration(
+        //               hintText: context.loc.start_typing_your_note),
+        //         );
+        //       default:
+        //         return const Center(
+        //           child: CircularProgressIndicator(),
+        //         );
+        //     }
+        //   },
+        // ),
+        );
   }
 }
