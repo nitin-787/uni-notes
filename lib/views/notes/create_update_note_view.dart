fe54382194d514d32d4a30f211628fb3fba67401
Nitin Sharma <nitinsharmaa787@gmail.com>
2022-11-08 13:40:59 +0530

    File upload in createUpdateNoteView

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mynotes/config/size_config.dart';
+import 'package:mynotes/constants/colors.dart';
 import 'package:mynotes/extentions/buildcontext/loc.dart';
 import 'package:mynotes/services/auth/auth_serivce.dart';
 import 'package:mynotes/utilities/dialogs/cannot_share_empty_not_dialog.dart';
@@ -18,6 +28,10 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   CloudNote? _note;
   late final FirebaseCloudStorage _notesSerivce;
   late final TextEditingController _textController;
+  File? file;
+  ImagePicker image = ImagePicker();
+  String url = "";
+  var name = "";
 
   @override
   void initState() {
@@ -94,6 +108,11 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // back icon
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
         title: Text(
           context.loc.note,
         ),
@@ -117,12 +136,54 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
               _setupTextControllerListner();
-              return TextField(
-                controller: _textController,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                    hintText: context.loc.start_typing_your_note),
+              return Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _textController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                          hintText: context.loc.start_typing_your_note),
+                    ),
+                    const Spacer(
+                      flex: 2,
+                    ),
+                    SizedBox(
+                      height: screenWidth(29.7),
+                      width: screenWidth(170.7),
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: AppColors.mainColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(screenHeight(20.86)),
+                          ),
+                        ),
+                        onPressed: () async {
+                          await getfile();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: screenWidth(9.86),
+                            ),
+                            Text(
+                              "Upload Note",
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: screenWidth(12.51),
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               );
             default:
               return const Center(
@@ -132,5 +193,59 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
         },
       ),
     );
+  }
+
+  getfile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'doc'],
+    );
+
+    if (result != null) {
+      File c = File(result.files.single.path!);
+      setState(() {
+        file = c;
+        name = result.names.toString();
+      });
+      uploadFile();
+    }
+  }
+
+  getImage() async {
+    var img = await image.pickImage(source: ImageSource.gallery);
+    setState(() {
+      file = File(img!.path);
+    });
+    if (file != null) {
+      uploadFile();
+    }
+  }
+
+  uploadFile() async {
+    try {
+      var imagefile =
+          FirebaseStorage.instance.ref().child("Users").child("/$name");
+      UploadTask task = imagefile.putFile(file!);
+      TaskSnapshot snapshot = await task;
+      url = await snapshot.ref.getDownloadURL();
+
+      print(url);
+      if (file != null) {
+        Fluttertoast.showToast(
+          msg: "file uloaded sucessfully",
+          textColor: Colors.grey,
+        );
+      } else {
+        Fluttertoast.showToast(
+          msg: "Something went wrong",
+          textColor: Colors.red,
+        );
+      }
+    } on Exception catch (e) {
+      Fluttertoast.showToast(
+        msg: e.toString(),
+        textColor: Colors.grey,
+      );
+    }
   }
 }
